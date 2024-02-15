27b1fd7c0ce48be4eaaa3f7484737031df856809
Spidey Mamu!! <141708771+saiteja0987@users.noreply.github.com>
2024-02-15 16:29:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PUBLIC_PORT || 3000;
 
-// Define the route for /ping with the response in JSON
+// define the ping route with the response in JSON
 app.get('/', (req, res) => {
-  res.json({ message: 'pong' }); 
+  res.json({message:'pong'});
 });
 
 if (require.main === module) {
   app.listen(port, () => {
-    console.log(`🚀 server running on PORT: ${port}`);
+    console.log(🚀 server running on PORT: ${port});
   });
 }
 
