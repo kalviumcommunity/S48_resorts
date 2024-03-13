a0c7cc88330a87d6c672e6968f5c5f2775befb05
Sai Teja <saitejapubglite@gmail.com>
2024-03-13 16:20:58 +0530

    added UpdateUser data

--- Frontend/usre/src/components/UpdateUser.jsx
+++ Frontend/usre/src/components/UpdateUser.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import './UpdateUser.css';
+// import './UpdateUser.css';
 
 function UpdateUser() {
     const { id } = useParams();
