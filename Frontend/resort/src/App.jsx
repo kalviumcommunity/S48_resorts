62131d69ef2489f7744f6a936d93173477e5a2e1
Sai Teja <saitejapubglite@gmail.com>
2024-04-20 22:44:50 +0530

    added landing page for project

--- Frontend/resort/src/App.jsx
+++ Frontend/resort/src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import './App.css';
+import Landingpage from './components/Landingpage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes and Route from react-router-dom
 
 function App() {
-
   return (
-   <div>
-    <h1>Welcome to our page!</h1>
-   </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landingpage />} /> {/* Define the route for the Landingpage component */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
