1d0370dd93325c03a73e7713db3acabe9e96f8a8
Sai Teja <saitejapubglite@gmail.com>
2024-04-24 22:18:51 +0530

    added needed code into project

--- Frontend/resort/src/components/Landingpage.jsx
+++ Frontend/resort/src/components/Landingpage.jsx
@@ -5,20 +5,25 @@ import './Landingpage.css';
 
 const Landingpage = () => {
   return (
-    <div className="landing-page">
-      <nav className="navbar">
-        <Link to="/login" className="button">Login</Link>
-        <Link to="/Data" className="button">Data</Link>
-        <div className="animation start-home"></div>
+    <div className="landing-page"> 
+      <nav className="navLanding">
+        <Link to="/login" className='navitms'>Login</Link>
+        <Link to="/signup" className='navitms'>Signup</Link>
       </nav>
+
       <header className="header">
         <div className="title">
-          <h1>Discover Serenity: Your Escape Awaits at Our Luxurious Resort</h1>
-          <div className="description">
-            <p>Welcome to our resort website! Explore our accommodations, dining options, activities, and more to plan your perfect getaway. From cozy rooms to exciting adventures, we have everything you need for an unforgettable stay. Book your escape today and let the relaxation begin!</p>
-          </div>
+          <h1>Your Escape Awaits at Our Luxurious Resorts</h1>
         </div>
       </header>
+      <div>
+        <div className="description">
+          <p>Welcome to our resort website! <br/> Explore our website which is filled with a lot of resorts, select the best you want. We have everything you need  to plan your perfect getaway. Book your escape today and let the relaxation begin!</p>
+        </div>
+        <div>
+          <button id='startbtn'>Explore -> </button>
+        </div>
+      </div>   
     </div>
   );
 }
