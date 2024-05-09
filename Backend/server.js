8e5a716e4ee1203e53013d232ad04675348b76f3
Spidey Mamu!! <141708771+saiteja0987@users.noreply.github.com>
2024-05-09 11:45:05 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,8 @@ const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const mongoDBuri = process.env.MONGODB_URI;
 const bodyParser = require('body-parser');
-const cors = require('cors'); 
-const UserModel = require('./model/Users');
 
 app.use(bodyParser.json());
-app.use(cors());
 
 mongoose.connect(mongoDBuri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -17,15 +14,11 @@ mongoose.connect(mongoDBuri, { useNewUrlParser: true, useUnifiedTopology: true }
   })
   .catch((err) => console.error(err));
 
-app.get('/getUsers', async (req, res) => {
-  try {
-    const users = await UserModel.find();
-    res.json(users);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
+app.get('/', (req, res) =>{
+  res.send("Server is running successfully")
+})
+
+
 
 app.listen(port, () => {
   console.log(`🚀Server is running on port ${port}`);
