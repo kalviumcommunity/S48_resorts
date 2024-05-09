3fd4f37d6968b37eeb722867852cd8a1ccfc7efa
Spidey Mamu!! <141708771+saiteja0987@users.noreply.github.com>
2024-05-09 11:48:27 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,32 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 const mongoose = require('mongoose');
-const mongoDBuri = process.env.MONGODB_URI;
-const bodyParser = require('body-parser');
+const mongoDBuri = process.env.MONGODB_URI
+const routes = require('./routes');
 
-app.use(bodyParser.json());
+// Mount the routes from routes.js
+app.use('/', routes);
 
-mongoose.connect(mongoDBuri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Database Connected');
-  })
-  .catch((err) => console.error(err));
+// connecting database(mongoDB) to server
+mongoose.connect(mongoDBuri)
+.then(()=>{console.log("Database Connected ")})
+.catch((err)=> console.error(err))
 
-app.get('/', (req, res) =>{
-  res.send("Server is running successfully")
+app.get("/", (req, res) => {
+    res.json(`Server is running on ${port}`)
 })
 
-
+//get request for mongodb
+app.get("/mongoConn", (req, res) => {
+    if (mongoose.connection.readyState == 1) {
+        res.json("Database Connected ")
+    }else{
+        res.json("Error connecting to Database")
+    }
+})
 
 app.listen(port, () => {
-  console.log(`🚀Server is running on port ${port}`);
-});
+    console.log(`🚀Server is running on port ${port}`);
+  });
