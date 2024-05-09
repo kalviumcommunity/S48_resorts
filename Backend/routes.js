c638de585e7d776ff93c791e5c5b93fc8636214f
Spidey Mamu!! <141708771+saiteja0987@users.noreply.github.com>
2024-05-09 11:45:19 +0530

    Update routes.js

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,68 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-let users = [
-    {
-        id:1,
-        name:"b.sai teja",
-        email:"saiteja99722@gmail.com"
-    }
-]; // Example data (you would typically use a database)
+let items = []; // Example data (you would typically use a database)
 
 // Create operation - POST
-router.post('/users', (req, res) => {
-    const {name,email} = req.body; 
-    if (!name || !email){ 
-        return res.status(404).json({message: "Required details are not provided"});
-    }
-    const newUser = {
-        id: users.length +1,
-        name: name,
-        email: email
-    }
-    users.push(newUser);
-    res.status(201).json(newUser);
+router.post('/items', (req, res) => {
+    const newItem = req.body;
+    items.push(newItem);
+    res.status(201).json(newItem);
 });
 
 // Read operation - GET
-router.get('/users', (req, res) => {
-    res.status(200).json(users);
+router.get('/items', (req, res) => {
+    res.json(items);
 });
-// get req to read spcfic user data
-router.get('/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id)   
-    const user = users.find(user => user.id === userId)
-    if (!user){
-        return res.status(404).json({
-            message:"User not found"
-        })
-    }
-    res.status(200).json(users);
-});
+
 // Update operation - PUT
-router.put('/users/:id', (req, res) => {
-    const userId =parseInt(req.params.id);
-    const {name,email} = req.body
-    const user = users.find(user=> user.id === userId)
-    if(!user){
-        return res.status(404).json({message:"User not found"})
-    }
-    user.name = name || user.name
-    user.email = email || user.email
-    
-    res.status(200).json(user);
+router.put('/items/:id', (req, res) => {
+    const itemId = req.params.index;
+    const updatedItem = req.body;
+    items[itemId] = updatedItem;
+    res.json(updatedItem);
 });
 
 // Delete operation - DELETE
-router.delete('/users/:id', (req, res) => {
-    const userId =parseInt(req.params.id);
-    const index = users.findIndex(user=> user.id === userId)
-    if (index === -1){
-        return res.status(404).json({message:"User not found"})
-    }
-    users.splice(index, 1);
-    // res.sendStatus(204)
-    res.json("deleted item successfully");
+router.delete('/items/:id', (req, res) => {
+    const itemId = req.params.id;
+    items.splice(itemId, 1);
+    res.sendStatus(204).json("deleted item successfully");
 });
 
 module.exports = router;
